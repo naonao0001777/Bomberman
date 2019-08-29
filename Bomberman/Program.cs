78e19e95e0047b2ee289ad7d68084b1e602142c0
abikonao <abikonao@fsi.co.jp>
2019-08-29 11:12:02 +0900

    爆弾配置の処理追加

--- Bomberman/Program.cs
+++ Bomberman/Program.cs
@@ -10,17 +10,17 @@ namespace Bomberman
     {
         public const int MAX_HEIGHT = 13;
         public const int MAX_WIDTH = 31;
-
-        enum cellAA
+        
+        enum CellAA
         {
-            CELL_TYPE_NONE, // "　"
-            CELL_TYPE_HARD, // "■"
-            CELL_TYPE_SOFT, // "□"
-            CELL_TYPE_BOMB, // "●"
-            PLAYER,         // "＠"
-            MONSTER         // "敵"
+            CELL_TYPE_NONE, 
+            CELL_TYPE_HARD, 
+            CELL_TYPE_SOFT, 
+            CELL_TYPE_BOMB, 
+            PLAYER,         
+            MONSTER         
         }
-       
+
         static void Main(string[] args)
         {
             int[,] cells = new int[MAX_HEIGHT, MAX_WIDTH];
@@ -31,6 +31,10 @@ namespace Bomberman
             int cursorY = 1;
             int cursorX = 1;
 
+            int bombTimer = 0;
+            bool bombFlg = false;
+
+            // リセット処理
             ResetCells();
 
             while (true)
@@ -40,9 +44,9 @@ namespace Bomberman
 
             }
             
-            // セル情報
+            // セル配置の初期情報
             void ResetCells()
-            {  
+            {
                 Random rY = new Random();
                 Random rX = new Random();
 
@@ -52,34 +56,44 @@ namespace Bomberman
                     {
                         if (x == cursorX && y == cursorY)
                         {
-                            cells[y, x] = (int)cellAA.PLAYER;
+                            cells[y, x] = (int)CellAA.PLAYER;
                         }
                         else if (y == 0 || y == MAX_HEIGHT - 1)
                         {
-                            cells[y, x] = (int)cellAA.CELL_TYPE_HARD;
+                            cells[y, x] = (int)CellAA.CELL_TYPE_HARD;
                         }
                         else if (x == 0 || x == MAX_WIDTH - 1)
                         {
-                            cells[y, x] = (int)cellAA.CELL_TYPE_HARD;
+                            cells[y, x] = (int)CellAA.CELL_TYPE_HARD;
                         }
                         else if (y % 2 == 0 && x % 2 == 0)
                         {
-                            cells[y, x] = (int)cellAA.CELL_TYPE_HARD;
+                            cells[y, x] = (int)CellAA.CELL_TYPE_HARD;
                         }
                         else
                         {
-                            cells[y, x] = (int)cellAA.CELL_TYPE_NONE;
+                            cells[y, x] = (int)CellAA.CELL_TYPE_NONE;
                         }
 
-                        if (cells[y, x] == (int)cellAA.CELL_TYPE_NONE)
+                        // CELL_TYPE_SOFTをランダム配置
+                        //if (cells[y, x] == (int)CellAA.CELL_TYPE_NONE)
+                        //{
+                        //    cells[rY.Next(13), rX.Next(31)] = (int)CellAA.CELL_TYPE_SOFT;
+                        //}
+                        // モンスターを配置する
+                        if (cells[y,x] == (int)CellAA.CELL_TYPE_NONE)
                         {
-                            cells[rY.Next(13), rX.Next(31)] = (int)cellAA.CELL_TYPE_SOFT;
+
+                            Random randMonstY = new Random();
+                            Random randMonstX = new Random();
+
+                            cells[randMonstY.Next(13), randMonstX.Next(31)] = (int)CellAA.MONSTER;
                         }
                     }
                     Console.Write("\n");
                 }
             }
-
+            
             // 画面を作成
             void DrawCells()
             {
@@ -87,7 +101,7 @@ namespace Bomberman
                 {
                     for (x = 0; x < MAX_WIDTH; x++)
                     {
-                        if (cells[y, x] != (int)cellAA.CELL_TYPE_BOMB)
+                        if (cells[y, x] != (int)CellAA.CELL_TYPE_BOMB)
                         {
                             
                             if (x == cursorX && y == cursorY)
@@ -108,9 +122,13 @@ namespace Bomberman
                             }
                             else
                             {
-                                if (cells[y, x] == (int)cellAA.CELL_TYPE_SOFT)
+                                if (cells[y, x] == (int)CellAA.CELL_TYPE_SOFT)
                                 {
                                     Console.Write("□");
+                                }
+                                else if (cells[y,x] == (int)CellAA.MONSTER)
+                                {
+                                    Console.Write("敵");
                                 }
                                 else
                                 {
@@ -118,21 +136,32 @@ namespace Bomberman
                                 }
                             }
                         }
-                        else if(cells[y, x] == (int)cellAA.CELL_TYPE_BOMB)
+                        else if(cells[y, x] == (int)CellAA.CELL_TYPE_BOMB)
                         {
-                            Console.Write("●");
+                            DateTime dt = DateTime.Now;
+                            bombTimer -= dt.Second;
+                            if (bombFlg&&bombTimer != 0)
+                            {
+                                Console.Write("●");
+                            }
+                            else if(bombFlg && bombTimer == 0)
+                            {
+                                cells[y, x] = (int)CellAA.CELL_TYPE_NONE;
+                            }
                         }
                     }
                     Console.Write("\n");
                 }
 
+                // カーソル移動
                 MoveCursor();
+               
 
             }
-            // カーソル移動
+
+            // カーソル操作処理
             void MoveCursor()
             {
-
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
                 string key = keyInfo.Key.ToString();
 
@@ -140,34 +169,37 @@ namespace Bomberman
                 {
                     case "W":
                         cursorY--;
-                        if (cells[cursorY, cursorX] == (int)cellAA.CELL_TYPE_HARD)
+                        if (cells[cursorY, cursorX] == (int)CellAA.CELL_TYPE_HARD)
                         {
                             cursorY++;
                         }
                         break;
                     case "S":
                         cursorY++;
-                        if (cells[cursorY, cursorX] == (int)cellAA.CELL_TYPE_HARD)
+                        if (cells[cursorY, cursorX] == (int)CellAA.CELL_TYPE_HARD)
                         {
                             cursorY--;
                         }
                         break;
                     case "A":
                         cursorX--;
-                        if (cells[cursorY, cursorX] == (int)cellAA.CELL_TYPE_HARD)
+                        if (cells[cursorY, cursorX] == (int)CellAA.CELL_TYPE_HARD)
                         {
                             cursorX++;
                         }
                         break;
                     case "D":
                         cursorX++;
-                        if (cells[cursorY, cursorX] == (int)cellAA.CELL_TYPE_HARD)
+                        if (cells[cursorY, cursorX] == (int)CellAA.CELL_TYPE_HARD)
                         {
                             cursorX--;
                         }
                         break;
                     default:
-                        cells[cursorY, cursorX] = (int)cellAA.CELL_TYPE_BOMB;
+                        cells[cursorY, cursorX] = (int)CellAA.CELL_TYPE_BOMB;
+                        DateTime date = DateTime.Now;
+                        bombTimer = date.Second + 3;
+                        bombFlg = true;
                         break;
                 }
 
